b16ba9a75a4bcbed7be2fa18069255920488c880
Polini <43158129+pilot-xls@users.noreply.github.com>
2025-05-13 20:34:02 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Nome da cache (altera para forçar atualização quando modificares ficheiros)
-const CACHE_NAME = 'calc-cache-v9';
+const CACHE_NAME = 'calc-cache-v10';
 
 // Lista de ficheiros a guardar para acesso offline
 const FILES_TO_CACHE = [

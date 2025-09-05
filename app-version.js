5e3a68f7d826d7ae661f25cde208b81865396442
Polini <43158129+pilot-xls@users.noreply.github.com>
2025-09-05 10:44:32 +0100

    906

--- app-version.js
+++ app-version.js
@@ -1,2 +1,9 @@
 // Versão da app (manual)
 const APP_VERSION = 'v906';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const footerVersion = document.getElementById("versaoApp");
+  if (footerVersion) {
+    footerVersion.textContent = APP_VERSION;
+  }
+});
